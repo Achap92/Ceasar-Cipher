71b90e00dd631d59731019b6cc5cbd403c1dcce7
Alex Chapman <37310790+Achap92@users.noreply.github.com>
2021-11-29 23:26:53 +0000

    Finished CSS file

---
+++ script.js
@@ -0,0 +1,11 @@
+// set up variables
+var key = 5;
+var range = 26;
+var aCode = 65;
+var zCode = aCode + range;
+
+// get HTML elements
+var input = document.getElementById("in");
+var encrypt = document.getElementById("encrypt");
+var decrypt = document.getElementById("decrypt");
+var output = document.getElementById("out");
