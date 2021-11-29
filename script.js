870c3d05aeda55274e8773c08f1036f9197a8168
Alex Chapman <37310790+Achap92@users.noreply.github.com>
2021-11-29 23:46:15 +0000

    Implemented Cipher. Needs debugging.

--- script.js
+++ script.js
@@ -9,3 +9,48 @@ var input = document.getElementById("in");
 var encrypt = document.getElementById("encrypt");
 var decrypt = document.getElementById("decrypt");
 var output = document.getElementById("out");
+
+encrypt.onclick = encipher;
+decrypt.onclick = decipher;
+
+function encipher() {
+  var message = input.value.toUpperCase();
+  output.innerHTML = "";
+
+  for (var i = 0; i < message.length; i++) {
+
+    var letter = message.charCodeAt(i);
+    var newLetter = letter;
+
+    if (letter >= aCode && letter <= zCode) {
+      newLetter += key;
+
+      if (newLetter >= zCode) {
+        newLetter -= range;
+      }
+    }
+    newLetter = String.fromCharCode(newLetter);
+    output.innerHTML += newLetter;
+  }
+}
+
+function decipher() {
+  var message = input.value.toUpperCase();
+  output.innerHTML = "";
+
+  for (var i = 0; i < message.length; i++) {
+
+    var letter = message.charCodeAt(i);
+    var newLetter = letter;
+
+    if (letter >= aCode && letter <= zCode) {
+      newLetter -= key;
+
+      if (newLetter >= zCode) {
+        newLetter += range;
+      }
+    }
+    newLetter = String.fromCharCode(newLetter);
+    output.innerHTML += newLetter;
+  }
+}
